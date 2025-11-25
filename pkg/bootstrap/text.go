c2854fc6ec631a087b1acba7b59f80ee64a47076
David Thorpe <djt@mutablelogic.com>
2025-11-25 08:28:44 +0100

    Added text

---
+++ pkg/bootstrap/text.go
@@ -0,0 +1,91 @@
+package bootstrap
+
+import (
+	// Package imports
+	dom "github.com/djthorpe/go-wasmbuild"
+	mvc "github.com/djthorpe/go-wasmbuild/pkg/mvc"
+)
+
+///////////////////////////////////////////////////////////////////////////////
+// TYPES
+
+type text struct {
+	mvc.View
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// GLOBALS
+
+const (
+	templateBlockquote = `
+	<figure>
+		<blockquote class="blockquote"><slot></slot></blockquote>
+		<figcaption class="blockquote-footer"><slot name="label"></slot></figcaption>
+	</figure>
+	`
+)
+
+func init() {
+	mvc.RegisterView(ViewText, func(element dom.Element) mvc.View {
+		return mvc.NewViewWithElement(new(text), element, func(self, child mvc.View) {
+			self.(*text).View = child
+		})
+	})
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// LIFECYCLE
+
+func Para(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "P", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func LeadPara(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "P", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, mvc.WithClass("lead"), args).(*text)
+}
+
+func Deleted(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "DEL", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Highlighted(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "MARK", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Smaller(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "SMALL", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Strong(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "STRONG", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Em(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "EM", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Blockquote(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, templateBlockquote, func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
+
+func Code(args ...any) *text {
+	return mvc.NewView(new(text), ViewText, "CODE", func(self, child mvc.View) {
+		self.(*text).View = child
+	}, args).(*text)
+}
