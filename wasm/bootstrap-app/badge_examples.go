64925e4bcd290ea18a8fec1c9714d2d913c908bc
David Thorpe <djt@mutablelogic.com>
2025-11-28 07:52:29 +0100

    Commit badge examples

--- wasm/bootstrap-app/badge_examples.go
+++ wasm/bootstrap-app/badge_examples.go
@@ -11,5 +11,20 @@ func BadgeExamples() mvc.View {
 		mvc.WithClass("my-3"),
 		Markdown("badge_examples.md"),
 		bs.HRule(),
+		Example(Example_Badges_001),
 	)
+}
+
+// Example_Badges_001 demonstrates badges in headings, with variations
+// on color, badge style and borders
+func Example_Badges_001() (mvc.View, string) {
+	return bs.Container(
+		bs.Heading(4, "Heading with default badge ", bs.Badge("1")),
+		bs.Heading(4, "Green badge ", bs.Badge("2", bs.WithColor(bs.Success))),
+		bs.Heading(4, "Heading with pill badge ", bs.PillBadge("3")),
+		bs.Heading(4, "Heading with secondary badge ", bs.Badge("4", bs.WithColor(bs.Secondary))),
+		bs.Heading(4, "Heading with badge with border ", bs.Badge("5", bs.WithBorder(bs.Black))),
+		bs.Heading(4, "Heading with danger badge ", bs.Badge("6", bs.WithColor(bs.Danger))),
+		bs.Heading(4, "Heading with outlined badge ", bs.Badge("6", bs.WithColor(bs.Light), bs.WithBorder(bs.Danger))),
+	), sourcecode()
 }
--- wasm/bootstrap-app/badge_examples.md
+++ wasm/bootstrap-app/badge_examples.md
@@ -1,2 +1,12 @@
 
 # Badges
+
+Badges scale to match the size of the immediate parent element by using relative font sizing. There
+are two types of badges: regular badges created with `bs.Badge()` and pill badges created with `bs.PillBadge()`, which
+are more rounded.
+
+Options for badges include:
+
+* `WithColor(bs.Color)` - set the background color
+
+The [Bootstrap documentation on Badges](https://getbootstrap.com/docs/5.3/components/badge/) provides more details and examples.
